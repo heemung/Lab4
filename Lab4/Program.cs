5ad7685de929683243ff7921046980b6dd8bd3ab
Clayton <clayton.cox@gmail.com>
2018-10-10 14:44:29 -0400

    Logic Done.

--- Lab4/Program.cs
+++ Lab4/Program.cs
@@ -4,18 +4,38 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace Lab4
 {
     class Program
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            //delcaring variables
+            double userInput;
+            int startingNumber = 1;
+            double squared;
+            double cubed;
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            do
+            {
+                Console.WriteLine("Learn your squares and cubes!");
+                Console.WriteLine("Enter an integer");
+                userInput = double.Parse(Console.ReadLine());
+
+                //Console.WriteLine(cubed);
+
+                Console.WriteLine("Number     Squared     Cubed\n======     " +
+                    "=======     =====\n");
+                for (int i = startingNumber; i <= userInput; i++)
+                {
+                    squared = Math.Pow(i, 2);
+                    cubed = Math.Pow(i, 3);
+                    Console.WriteLine("{0}      {1}            {2}", i, squared,
+                        cubed);
+                }
+                Console.WriteLine("Do You Want To Continue. (y/n)?");
+            } while (Console.ReadLine().ToLower() == "y");
         }
     }
 }
